a9457dd207f4d9fcb12e6698bdaa0206879321df
Allusion001 <muhammadalispear@gmail.com>
2025-03-26 04:28:48 -0500

    final

--- backend/weatherApp/weatherApp/settings.py
+++ backend/weatherApp/weatherApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-a+f4v(v&p+#)c8i3je=5xf2yjx48^57%gs$j0dq0xsy5_^+0&9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
